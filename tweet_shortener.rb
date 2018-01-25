ae0e9579f8f1beadac10acdc19b56eb068f0cf4d
danl411412 <daan.b.lee@gmail.com>
2018-01-25 08:07:59 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+def dictionary
+  substitues = {
+    "hello" => "hi",
+    "to" => "2",
+    "too" => "2",
+    "two" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet) 
+  shorter_tweet = []
+  tweet.split.collect do |word|
+    if dictionary.keys.include?(word)
+      shorter_tweet << dictionary[word]
+    else
+      shorter_tweet << word  
+    end
+  end
+  shorter_tweet.join(" ")
+end
